9a5c98a28018c53da28578a8ca80fc9d70e8c2ac
Mihir Singh <20bcs131@iiitdmj.ac.in>
2023-04-08 19:41:18 +0530

    add comment

--- FusionIIIT/applications/gymkhana/clubfeeds/urls.py
+++ FusionIIIT/applications/gymkhana/clubfeeds/urls.py
@@ -7,4 +7,6 @@ app_name = 'clubfeed'
 urlpatterns = [
     path('addPost/', views.addPost , name='addPost'),
     path('like/<int:post_id>/', views.like, name='like'),
+    # path('posts/<int:post_id>/', views.show_comment, name='show_comment'),
+    path('addcomment/<int:post_id>/', views.add_comment, name='addcomment')
 ]
